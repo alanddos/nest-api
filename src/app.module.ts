4f706595a8d647704a86decfcacf5a3ebf0c7e52
alanddos <35971387+alanddos@users.noreply.github.com>
2020-02-12 13:58:37 -0400

    Banco de dados conectado, criar um modulo de fotos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//modulo de autenticação
 import { AuthModule } from './auth/auth.module';
+
+//modulo de usuarios
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service'
 
+//conexao com o banco da dados
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot()],//forRoot sem paramentos busca do ormconfig.json
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private readonly connection: Connection) {}  
+ }
