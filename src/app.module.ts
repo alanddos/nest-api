64ba7071bd9c54aaeb75f06f3986375d1359b504
Alan <alanddos@gmail.com>
2020-02-12 09:01:05 -0400

    modulos iniciais de login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UsersnestService } from './g/usersnest/usersnest.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersnestService],
 })
 export class AppModule {}
